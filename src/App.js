f790007c121a7548d9c69c4d365de64ae2112e0f
Justin Wozniak <wozzytheprogrammer@gmail.com>
2020-01-12 22:25:38 -0800

    First commit. Got app to render and did css

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+
+function App()  {
+    return(
+        <div>
+        <h1>Super Typer 4000</h1>
+        <textarea />
+        <h4>Time Remaining: </h4>
+        <button>Start Game</button>
+        <h1>Word Count: </h1>
+        </div>
+    )
+}
+export default App
